76f5869573e3764fb0644620798b4b197ac7dc43
Nguyen Ngo <nqngo@ucdavis.edu>
2016-11-28 02:45:02 -0800

    Added prompt for settings

--- src/team9/Algorithm.java
+++ src/team9/Algorithm.java
@@ -1,43 +1,105 @@
 package team9;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import com.vmware.vim25.*;
 import com.vmware.vim25.mo.*;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 
 public class Algorithm {
-	public static void main(String[] args) throws Exception
-	{
-		ServiceInstance si = new ServiceInstance(new URL("https://" + args[0] + "/sdk"), args[1], args[2], true);
-        Folder rootFolder = si.getRootFolder();
-        ResourcePool rp = (ResourcePool) new InventoryNavigator(rootFolder).searchManagedEntity("ResourcePool", "Resources");
-      //  System.out.println("RESP = " + rp.getName());
-        //String hostname = "130.65.159.153";
-        //ManagedEntity me = new InventoryNavigator(rootFolder).searchManagedEntity("HostSystem", hostname);
-        ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");	
+	ServiceInstance si;
+	Integer cpuThreshold;
+	
+	private void migrate(Scanner scanner) throws RemoteException, InterruptedException{
+		Integer currentHost;
+		Integer currentVM;
+		Integer newHost;
+		System.out.println("Migrate vm: <currentHost> <currentVM> <newHost>:");
+		currentHost = scanner.nextInt();
+		currentVM = scanner.nextInt();
+		newHost = scanner.nextInt();
+		//get rootFolder
+		Folder rootFolder = si.getRootFolder();
+		//get Array of all hosts
+		ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");
+		ComputeResource cr = (ComputeResource)mes[newHost].getParent();
+		//get target resource pool (new host's resource pool)
+		ResourcePool rp = cr.getResourcePool();
+		migrateVM(rp, ((HostSystem)mes[currentHost]).getVms()[currentVM], ((HostSystem)mes[newHost]) );
+		
+	}
+	
+	private void printHostsAndVMs() throws RemoteException{
+		Folder rootFolder = si.getRootFolder();
+		ManagedEntity[] mes = new InventoryNavigator(rootFolder).searchManagedEntities("HostSystem");
 		for(int i = 0; i < mes.length; i++){
 			HostSystem hs = (HostSystem)mes[i];
 			int usedCpu = hs.getSummary().getQuickStats().getOverallCpuUsage();
 			System.out.println("Host: " + hs.getName() + ", CPU usage: " + usedCpu);
-		}
-		int i = 0;
-		ManagedEntity[] meVM = new InventoryNavigator(rootFolder).searchManagedEntities("VirtualMachine");
-		if (meVM == null || meVM.length == 0) 
-			System.out.println("No virtual machine!");
-		else
-		{
-			HostSystem hs1 = (HostSystem)mes[0];
-			HostSystem hs2 = (HostSystem)mes[1];
-			HostSystem hs3 = (HostSystem)mes[2];
-			VirtualMachine[] vm = new VirtualMachine[meVM.length];
-			for (ManagedEntity me: meVM)
-			{
-				vm[i++] = (VirtualMachine) me;
-				System.out.println("VM " + i + " = " + vm[i-1].getName());
+			VirtualMachine[] vms = hs.getVms();
+			for(int j =0 ;j < vms.length; j++){
+				System.out.println("\tVM " + j + " = " + vms[j].getName() + " Power state: " + vms[j].getRuntime().getPowerState());
 			}
 		}
-        si.getServerConnection().logout();
+	}
+	
+	private void setCPUThreshold(){
+		System.out.println("Enter cpu threshold: ");
+		Scanner scanner = new Scanner(System.in);
+		cpuThreshold = scanner.nextInt();
+		scanner.close();
+	}
+	
+	private void run() throws RemoteException, InterruptedException {
+		Integer choice;
+		Scanner scanner = new Scanner(System.in);
+		do{
+			System.out.println("0. Print all hosts and their respective virtual machines\n"
+					+ "1. Migrate a virtual machine\n"
+					+ "2. Set cpu threshold");
+			choice = scanner.nextInt();
+			switch(choice){
+			case 0:
+				printHostsAndVMs();
+				break;
+			case 1:
+				migrate(scanner);
+				break;
+			case 2:
+				setCPUThreshold();
+				break;
+			default:
+				break;
+			}
+		}while(choice != 9);
+		scanner.close();
+	}
+	
+	private void closeConnection(){
+		si.getServerConnection().logout();
+	}
+	
+	public Algorithm(String IP, String login, String password){
+		System.out.println("IP: " + IP);
+		try{
+			si = new ServiceInstance(new URL("https://" + IP + "/sdk"), login, password, true);
+		} catch (RemoteException e){
+			System.err.println("RemoteException: " + e.getMessage());
+			System.exit(1);
+		} catch (MalformedURLException e){
+			System.err.println("MalformedURLException: " + e.getMessage());
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+        Algorithm algorithm = new Algorithm(args[0], args[1], args[2]);
+        algorithm.run();
+        algorithm.closeConnection();
+
 	}
 	
 	public boolean migrateVM(ResourcePool rp, VirtualMachine vm, HostSystem host) throws VmConfigFault, Timedout, FileFault, InvalidState, InsufficientResourcesFault, MigrationFault, RuntimeFault, RemoteException, InterruptedException
@@ -54,7 +116,7 @@ public class Algorithm {
 	      return false;
 	    }
 	}
-	public static boolean checkCompatible(ServiceInstance si, VirtualMachine vm, HostSystem host) throws RuntimeFault, RemoteException
+	public boolean checkCompatible(VirtualMachine vm, HostSystem host) throws RuntimeFault, RemoteException
 	{
 		String[] checks = new String[] {"cpu", "software"};
 		HostVMotionCompatibility[] vmcs = si.queryVMotionCompatibility(vm, new HostSystem[]{host},checks );
